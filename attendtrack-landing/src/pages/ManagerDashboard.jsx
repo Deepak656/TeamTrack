1f73e52d4a70454adcc751763410cfb4abf98dc3
Deepal Kumar <deepak01962@gmail.com>
2025-07-30 20:50:49 +0530

    Delete attendtrack-landing directory

--- attendtrack-landing/src/pages/ManagerDashboard.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { ManagerDashboard } from '../components/dashboard';
-
-const ManagerDashboardPage = () => {
-  return <ManagerDashboard />;
-};
-
-export default ManagerDashboardPage;
